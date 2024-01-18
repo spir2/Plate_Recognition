e8e9a5c12d2f2625f7353fc6cb63b9cc5b09f116
Sacha THIENPONT <130176259+spir2@users.noreply.github.com>
2024-01-18 16:03:41 +0100

    Final open_close_barriere.py

--- open_close_barriere.py
+++ open_close_barriere.py
@@ -2,6 +2,8 @@ import cv2
 import easyocr
 import matplotlib.pyplot as plt
 import re
+import time
+import serial
 
 class PlateDetector:
     def __init__(self, lang=['en'], gpu=True):
@@ -9,27 +11,26 @@ class PlateDetector:
         self.plate = str()
 
     def detect_and_display_text_live(self, threshold=0.20):
-        cap = cv2.VideoCapture(0)  
+        cap = cv2.VideoCapture(0)
 
         var = True
         while var:
             ret, frame = cap.read()
             if not ret:
-                print("Erreur : Impossible de lire le flux vidéo.")
                 break
 
             text_ = self.reader.readtext(frame)
             for bbox, text, score in text_:
-                print(text)
-                self.plate = self.clean_plate(text)
+                print("->",text)
+                #self.plate = self.clean_plate(text)
+                self.plate = text
                 if score > threshold and self.plate:
-                    print(f"--------> {self.plate} <--------")
+                    print(f"-------->{self.plate}<--------")
                     top_left = (int(bbox[0][0]), int(bbox[0][1]))
                     bottom_right = (int(bbox[2][0]), int(bbox[2][1]))
                     cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), 5)
                     cv2.putText(frame, text, top_left, cv2.FONT_HERSHEY_COMPLEX, 0.65, (255, 0, 0), 2)
-                    var=False
-            
+                    var = False
 
             cv2.imshow('Détection de texte en direct', frame)
 
@@ -47,33 +48,42 @@ class PlateDetector:
             return None
 
 class Barriere:
-    def __init__(self):
-        pass
+    def __init__(self, serial_port):
+        self.serial_port = serial.Serial(serial_port, 9600)  
 
-    def open_barriere():
+    def open_barriere(self):
         print("Open")
-        #mettre la fonction pour ouvrir la barriere 
-        
+        self.serial_port.write(b'OPEN\n')
 
-    def close_barriere():
-        #mettre la fonction pour fermer la barriere 
+
+    def close_barriere(self):
         print("Close")
+        self.serial_port.write(b'CLOSE\n')
 
-    def verif_barriere(verif):
-        L=["AB123CD","AD456KI","AA000AA","AA123AA"]
-        if verif in L: 
+    def verif_barriere(self, verif):
+        print(verif)
+        L = ["AB123CD",
+             "AD456KI",
+             "AA000AA",
+             "AA123AA",
+             "AB344CA",
+             "AA112AA",
+             "AA725AD",
+             "B2228HM"]
+        if verif in L:
             return True
         return False
-        
-#Mettre programme de detection de plaque qui renverra la valeur de la plaque en parametre de la fct verif_barriere()
+
 
 if __name__ == "__main__":
     detector = PlateDetector()
-    barriere = Barriere()
-    detector.detect_and_display_text_live()
-print("test",detector.plate)
-if Barriere.verif_barriere(detector.plate):
+    barriere = Barriere('COM4') 
+    boo = True
+    while boo:
+        detector.detect_and_display_text_live()
+        if barriere.verif_barriere(detector.plate):
+            barriere.open_barriere()
+        else:
+            barriere.close_barriere()
 
-    Barriere.open_barriere()
-else :
-    Barriere.close_barriere()
+        #time.sleep(2)
